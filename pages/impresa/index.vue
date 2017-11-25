4cebbdbbe18ffac16f4d73a79c89191ac77ebde2
Lorenzo Caldara <lorenzo@dinoweb.net>
2017-11-25 11:13:54 +0100

    Passi avanti gestione prismic

--- pages/impresa/index.vue
+++ pages/impresa/index.vue
@@ -1,17 +1,20 @@
 <template lang="html">
     <div>
-        <h1 v-for="articolo in articoli"  class="red">{{articolo.uid}}</h1>
+        <h1 v-for="articolo in articoli"  class="red"><span v-html="PrismicDom.RichText.asHtml(articolo.data.titolo)"></span></h1>
     </div>
 
 </template>
 
 <script>
 
+import PrismicDom from 'prismic-dom'
+
 export default {
     middleware: 'getArticoliByCategoria',
     data: function () {
         return {
-            articoli: this.$store.getters['articoli/getArticoli']
+            articoli: this.$store.getters['articoli/getArticoli'],
+            PrismicDom: PrismicDom
         }
     }
 }
