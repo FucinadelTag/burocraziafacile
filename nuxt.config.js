bdf3b172e44f3bd0f1e93b62b7a72d76f91956f2
Lorenzo Caldara <lorenzo@dinoweb.net>
2017-12-01 10:08:18 +0100

    Forza https

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,9 @@ module.exports = {
             }
         }
     },
+    serverMiddleware: [
+        'redirect-ssl'
+    ],
     router: {
         middleware: ['ActiveMenu', 'getSettings', 'getNews']
     },
