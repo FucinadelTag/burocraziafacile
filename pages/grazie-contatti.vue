3745b3ca164ebadc3198b223bc49947bb3de2eed
Lorenzo Caldara <lorenzo@dinoweb.net>
2017-12-06 16:50:37 +0100

    Creata pagina di grazie contatti

---
+++ pages/grazie-contatti.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div class="has-text-centered grazieNewsletter">
+        <h1 class="title is-1">Grazie!</h1>
+        <p class="subtitle is-3">
+            Messaggio ricevuto con Successo!
+        </p>
+        <p>
+            Ti risponderemo il più velocemente possibile.
+        </p>
+
+        <a href="/">Torna alla Home page del sito</a>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.grazieNewsletter {
+    padding: 4rem;
+
+    a {
+        font-size: 220%;
+    }
+}
+</style>
